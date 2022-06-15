c72ec0536df1b6ac4c69d9373afbf75c415e97a2
Adam Wight <adam.wight@wikimedia.de>
2022-06-15 10:36:21 +0200

    Restore internal mechanism to use either back or close button
    For some use cases of the transclusion dialog such as Insert
    Citation, the back button is inappropriate and we want to keep the
    close box.
    
    This partially reverts commit 2e2e40e76e081b94e83ceb465ab56190a1f5693e.
    
    Bug: T310602
    Change-Id: If68a822c260618c0a93eb8d0c46d2b452fee8baa

--- modules/ve-mw/ui/dialogs/ve.ui.MWTransclusionDialog.js
+++ modules/ve-mw/ui/dialogs/ve.ui.MWTransclusionDialog.js
@@ -31,6 +31,7 @@ ve.ui.MWTransclusionDialog = function VeUiMWTransclusionDialog( config ) {
 	// Temporary feature flags
 	this.useInlineDescriptions = veConfig.transclusionDialogInlineDescriptions;
 	this.useSearchImprovements = veConfig.templateSearchImprovements;
+	this.useBackButton = true;
 	this.useNewSidebar = veConfig.transclusionDialogNewSidebar;
 
 	if ( this.useInlineDescriptions ) {
@@ -542,11 +543,15 @@ ve.ui.MWTransclusionDialog.prototype.updateActionSet = function () {
 		saveButton.setLabel( ve.msg( 'visualeditor-dialog-transclusion-action-save' ) );
 	}
 
-	var closeButton = this.actions.get( { flags: [ 'close' ] } ).pop(),
-		canGoBack = this.getMode() === 'insert' && !this.transclusionModel.isEmpty();
+	if ( this.useBackButton ) {
+		var closeButton = this.actions.get( { flags: [ 'close' ] } ).pop(),
+			canGoBack = this.getMode() === 'insert' && !this.transclusionModel.isEmpty();
 
-	closeButton.toggle( !canGoBack );
-	backButton.toggle( canGoBack );
+		closeButton.toggle( !canGoBack );
+		backButton.toggle( canGoBack );
+	} else {
+		backButton.toggle( false );
+	}
 };
 
 /**
